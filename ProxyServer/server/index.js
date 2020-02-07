7ebf40d259fd47f8290b36ddc30e0b1c74ba51ac
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-02-07 13:10:01 -0700

    Setting up for the AWS proxy

--- ProxyServer/server/index.js
+++ ProxyServer/server/index.js
@@ -8,20 +8,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('./public'));
 
-// app.get('/images/:id', (req, res) => {
-//   const id = req.params.id
-//   // additional items
-//   axios.get(`http://localhost:3004/images/${id}`)
-//     .then((response) => {
-//       // console.log(response);
-//       res.status(200).json(response.data);
-//     });
-// });
+app.get('/carousel/:id', (req, res) => {
+  const id = req.params.id
+  // additional items
+  axios.get(`http://localhost:3001/carousel/${id}`)
+    .then((response) => {
+      // console.log(response);
+      res.status(200).json(response.data);
+    });
+});
 
 app.get('/details/:id', (req, res) => {
   const id = req.params.id
   // additional items
-  axios.get(`http://localhost:3002/details/${id}`)
+  axios.get(`http://ec2-18-220-152-98.us-east-2.compute.amazonaws.com/details/${id}`)
     .then((response) => {
       console.log(response);
       res.status(200).json(response.data);
@@ -31,7 +31,7 @@ app.get('/details/:id', (req, res) => {
 app.get('/reviews/:id', (req, res) => {
   const id = req.params.id
   // additional items
-  axios.get(`http://localhost:3003/reviews/${id}`)
+  axios.get(`http://ec2-3-14-246-138.us-east-2.compute.amazonaws.com/reviews/${id}`)
     .then((response) => {
       console.log(response);
       res.status(200).json(response.data);
@@ -41,7 +41,7 @@ app.get('/reviews/:id', (req, res) => {
 app.get('/additional/:id', (req, res) => {
   const id = req.params.id
   // additional items
-  axios.get(`http://localhost:3004/additional/${id}`)
+  axios.get(`http://ec2-3-133-117-113.us-east-2.compute.amazonaws.com/additional/${id}`)
     .then((response) => {
       // console.log(response);
       res.status(200).json(response.data);
